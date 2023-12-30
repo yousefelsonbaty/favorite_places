4d1dc1e42d8c934411d7db6abdbcfde0f2ea111e
Yousef Elsonbaty <126959659+yousefelsonbaty@users.noreply.github.com>
2023-12-30 23:45:11 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\VScodeWorkspace\\Flutter\\favorite_places" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\VScodeWorkspace\\Flutter\\favorite_places"
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\VScodeWorkspace\\Flutter\\favorite_places\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\VScodeWorkspace\\Flutter\\favorite_places"
+  "FLUTTER_TARGET=D:\\VScodeWorkspace\\Flutter\\favorite_places\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iMjAxODNlMDQwOTYwOTRiY2MzN2Q5Y2RlMmE0Yjk2ZjVjYzY4NGNmLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\VScodeWorkspace\\Flutter\\favorite_places\\.dart_tool\\package_config.json"
+)
